f8997c75e92e364fa4b147c97c025a44a7a2c29b
onofre catala castellar <onofrecc@gmail.com>
2022-09-29 13:33:15 +0200

    Technical challenge 20220929 solved

---
+++ 2022099_tc.rb
@@ -0,0 +1,29 @@
+# Challenge: Change numbers (1 - 100) containing 7 OR can be divisible by 7
+# Round of drinking game
+# Shout "JUF" for numbers containing 7 and is divisible by 7
+#
+# 1
+# 2
+# ..
+# 6
+# "JUF"
+# 8
+# ..
+# 13
+# "JUF"
+# 15
+
+# SOLUTION USING TERNARY OPERATOR
+(1..100).each do |num|
+  num.to_s.include?('7') || (num % 7).zero? ? (puts 'JUF') : (puts num)
+end
+
+# SOLUTION USING IF... ELSE STATEMENT
+(1..25).each do |num|
+  # 1. Number contains 7: make it into string, check if any of the elements is 7
+  if num.to_s.include?('7') || (num % 7).zero?
+    puts 'JUF'
+  else
+    puts num
+  end
+end
