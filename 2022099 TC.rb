bc769976458990945058d0941a6914f0ee5841cc
onofre catala castellar <onofrecc@gmail.com>
2022-09-29 13:02:59 +0200

    Starting the Technical challenges

---
+++ 2022099 TC.rb
@@ -0,0 +1,28 @@
+# Challenge: Change numbers (1 - 100) containing 7 OR can be divisible by 7
+
+# Round of drinking game
+# Shout "JUF" for numbers containing 7 and is divisible by 7
+#
+# 1
+# 2
+# ..
+# 6
+# "JUF"
+# 8
+# ..
+# 13
+# "JUF"
+# 15
+
+
+(1..100).each do |num|
+  1. Number contains 7
+  
+  num
+
+
+  if num % 7 == 0 ||
+  else
+    puts num
+  end
+end
